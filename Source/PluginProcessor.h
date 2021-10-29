bec6312e1f81f87998b15ed2fffa56ca6bf92352
Thomas Boggs <thomboggs@gmail.com>
2021-10-28 23:08:32 -0400

    Cleanup Code

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -60,15 +60,7 @@ public:
     
 private:
     CompressorBand compressor;
-//    juce::dsp::Compressor<float> compressor;
-//    
-//    juce::AudioParameterFloat* attack { nullptr };
-//    juce::AudioParameterFloat* release { nullptr };
-//    juce::AudioParameterFloat* threshold { nullptr };
-//    juce::AudioParameterChoice* ratio { nullptr };
-//    juce::AudioParameterBool* bypassed { nullptr };
-    
-    
+
     
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleMBCompAudioProcessor)
