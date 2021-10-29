bfabae707267e1b4608746a3ebcf2557e2ef0632
Thomas Boggs <thomboggs@gmail.com>
2021-10-28 22:58:40 -0400

    Add Compressor Params and APH save file

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -22,6 +22,21 @@ SimpleMBCompAudioProcessor::SimpleMBCompAudioProcessor()
                        )
 #endif
 {
+    attack = dynamic_cast<juce::AudioParameterFloat*>(apvts.getParameter("Attack"));
+    jassert(attack != nullptr);
+    
+    release = dynamic_cast<juce::AudioParameterFloat*>(apvts.getParameter("Release"));
+    jassert(release != nullptr);
+    
+    threshold = dynamic_cast<juce::AudioParameterFloat*>(apvts.getParameter("Threshold"));
+    jassert(threshold != nullptr);
+    
+    ratio = dynamic_cast<juce::AudioParameterChoice*>(apvts.getParameter("Ratio"));
+    jassert(ratio != nullptr);
+    
+    bypassed = dynamic_cast<juce::AudioParameterBool*>(apvts.getParameter("Bypassed"));
+    jassert(bypassed != nullptr);
+    
 }
 
 SimpleMBCompAudioProcessor::~SimpleMBCompAudioProcessor()
@@ -93,8 +108,12 @@ void SimpleMBCompAudioProcessor::changeProgramName (int index, const juce::Strin
 //==============================================================================
 void SimpleMBCompAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    juce::dsp::ProcessSpec spec;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = getTotalNumOutputChannels();
+    spec.sampleRate = sampleRate;
+    
+    compressor.prepare(spec);
 }
 
 void SimpleMBCompAudioProcessor::releaseResources()
@@ -143,19 +162,17 @@ void SimpleMBCompAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+    
+    compressor.setAttack(attack->get());
+    compressor.setRelease(release->get());
+    compressor.setThreshold(threshold->get());
+    compressor.setRatio(ratio->getCurrentChoiceName().getFloatValue());
+    
+    auto block = juce::dsp::AudioBlock<float> ( buffer );
+    auto context = juce::dsp::ProcessContextReplacing<float> ( block );
+    context.isBypassed = bypassed->get();
+    
+    compressor.process( context );
 }
 
 //==============================================================================
@@ -166,21 +183,66 @@ bool SimpleMBCompAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* SimpleMBCompAudioProcessor::createEditor()
 {
-    return new SimpleMBCompAudioProcessorEditor (*this);
+//    return new SimpleMBCompAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
 void SimpleMBCompAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);
 }
 
 void SimpleMBCompAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid())
+    {
+        apvts.replaceState(tree);
+    }
+}
+
+
+juce::AudioProcessorValueTreeState::ParameterLayout SimpleMBCompAudioProcessor::createParameterLayout()
+{
+    APVTS::ParameterLayout layout;
+    
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Threshold",
+                                                           "Threshold",
+                                                           juce::NormalisableRange<float>(-60.f, 12.f, 1.f, 1.f),
+                                                           0.f));
+    
+    auto attackReleaseRange =  juce::NormalisableRange<float>( 5.f, 500.f, 1.f, 1.f );
+    
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Attack",
+                                                           "Attack",
+                                                           attackReleaseRange,
+                                                           50.f));
+    
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Release",
+                                                           "Release",
+                                                           attackReleaseRange,
+                                                           250.f));
+    
+    juce::StringArray strArray;
+    std::vector<double> threshVec = {1.0, 2.0, 3.0, 5.0, 8.0, 13.0, 21.0, 34.0, 55.0, 89.0};
+    for (auto thresh : threshVec)
+    {
+        strArray.add(juce::String(thresh, 1));
+    }
+    
+    layout.add(std::make_unique<juce::AudioParameterChoice>("Ratio",
+                                                            "Ratio",
+                                                            strArray,
+                                                            2));
+    
+    layout.add(std::make_unique<juce::AudioParameterBool>("Bypassed",
+                                                          "Bypassed",
+                                                          false));
+    
+    
+    return layout;
 }
 
 //==============================================================================
